60b391217ac1abf3730da3f094465c69d0990397
Forrest Oliphant <forrest@sembiki.com>
2017-01-30 00:44:04 -0500

    try bel createElement and domcss

--- src/components/mapped-element.js
+++ src/components/mapped-element.js
@@ -1,19 +1,27 @@
+const el = require('yo-yo').createElement
+const domcss = require('dom-css')
 const {matrix3d} = require('../projection-mapping/projection-mapping')
 
 function mappedElement (item) {
   const {type, src, width, height, x1, y1, x2, y2, x3, y3, x4, y4} = item
 
-  const el = document.createElement(type)
-  el.src = src
-  el.width = width
-  el.height = height
-  el.style.position = 'absolute'
-  el.style.top = 0
-  el.style.left = 0
-  el.style.transform = matrix3d(width, height, x1, y1, x2, y2, x3, y3, x4, y4)
-  el.style.transformOrigin = '0 0'
+  const element = el(type, {
+    src,
+    width,
+    height,
+    border: 0,
+  })
 
-  return el
+  domcss(element, {
+    border: 'none',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    transformOrigin: '0 0',
+    transform: matrix3d(width, height, x1, y1, x2, y2, x3, y3, x4, y4),
+  })
+
+  return element
 }
 
 module.exports = mappedElement
